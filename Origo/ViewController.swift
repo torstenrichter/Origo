0be9987c735a3e34384aa7b0d15ddd7f831fede9
Torsten Richter <t.richter@sunrise.ch>
2018-09-06 13:12:35 +0200

    Initial Commit

---
+++ Origo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Origo
+//
+//  Created by Torsten Richter on 06.09.18.
+//  Copyright © 2018 Torsten Richter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
